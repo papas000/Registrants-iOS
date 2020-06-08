de1208c9a6fdbe8eba1d048dc229704693be1bd2
papas000 <papas000@gmail.com>
2020-06-09 01:09:14 +0200

    main

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "https://papas000.eu.pythonanywhere.com")!
+        webView.load(URLRequest(url: url))
     }
 
-
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+//    @IBAction func reloadView() {
+//        webView.reload()
+//    }
+//
+//    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+//        title = webView.title
+//    }
 }
 
